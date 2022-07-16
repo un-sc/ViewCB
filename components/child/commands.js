eb9b401f12f99969b29338edee1d4762f4063350
Lianan <2779799659@qq.com>
2022-07-16 11:55:50 +0800

    新增参与链接

--- components/child/commands.js
+++ components/child/commands.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Link from 'next/link'
 import CmdList from './cmd/list';
+import Link from 'next/link'
 
 export default function Commands() {
     return (
@@ -9,6 +9,10 @@ export default function Commands() {
                 <title>命令详解列表 - ViewCB 我的世界指令百科全书</title>
             </Head>
             <CmdList />
+            <hr />
+            <div className="alert alert-primary" role="alert">
+                更多指令正在编写中，如果您有意参与编写请<Link href="/contribute"><a className='redText'>参考此处</a></Link>
+            </div>
         </div>
     )
 }
