148690f70ba29041c5756cc9558c86057a4e3c2a
Lianan <2779799659@qq.com>
2022-08-02 10:19:37 +0800

    更改链接地址

--- components/child/contribute.js
+++ components/child/contribute.js
@@ -17,9 +17,9 @@ export default function Contribute() {
                     <p className="lead">ViewCB的大部分内容均由联安一人编写，工作量繁重。也许在文档中会出现语义错误、表述不明的情况，亦或是您认为内容结构、页面布局等方面有待改进······</p>
                     <p className="lead">我（联安）希望大家能够对ViewCB提出意见和建议，也更希望能有志同道合之士和我共同编写内容。</p>
                     <h3>如何提出修改意见或亲身参与编撰？</h3>
-                    <p className="lead">如果您发现本站的内容存在问题，欢迎您将指正意见发送至本人的邮箱：<Link href='mailto: 2779799659@qq.com'><a>2779799659@qq.com</a></Link></p>
+                    <p className="lead">您可以<Link href="https://www.wenjuan.com/s/UZBZJv18XE3/"><a target="_blank">前往此处</a></Link>提交您的宝贵意见、或者要补充的内容（如果内容过长您可以上传文件）。</p>
                     <h3>关于本站的文字编撰方法</h3>
-                    <p className='lead'>本站使用了<Link href="https://github.com/remarkjs/remark">remark</Link>库，以此来实现将markdown文件转化为html可识别内容。本站的命令详解部分内容均使用了该方法，因此文档的源文件为markdown文件。</p>
+                    <p className='lead'>本站使用了<Link href="https://github.com/remarkjs/remark"><a target="_blank">remark</a></Link>库，以此来实现将markdown文件转化为html可识别内容。本站的命令详解部分内容均使用了该方法，因此文档的源文件为markdown文件。</p>
                     <p className='lead'>您可以尝试为ViewCB自行编写markdown文件，提供不一样的内容风格。</p>
                     <h3>联系方式</h3>
                     <ul className='lead'>
