5dcd1454a1eea17370d5c5610d8612c28b10141d
Lianan <2779799659@qq.com>
2022-09-11 22:14:23 +0800

    新增备案号

--- components/footer.js
+++ components/footer.js
@@ -13,12 +13,18 @@ export default function Footer() {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link active">
-                                Copyright © 2022 ViewCB. All rights reserved. 
+                                Copyright © 2022 ViewCB. All rights reserved.
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" href='http://beian.miit.gov.cn/' target="_blank">
                                 冀ICP备2022016585号
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href='http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13108202000849' target="_blank">
+                                <img src='https://unsc.oss-accelerate.aliyuncs.com/ViewCB/Images/beian.png' />
+                                冀公网安备 13108202000849号
                             </a>
                         </li>
                     </ul>
