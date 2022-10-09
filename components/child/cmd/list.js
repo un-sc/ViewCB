4a6927294e86a1889ae0843a84ac5f1ccf6351c2
Lianan <2779799659@qq.com>
2022-10-10 00:03:37 +0800

    修订列表UI界面

--- components/child/cmd/list.js
+++ components/child/cmd/list.js
@@ -16,7 +16,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/alwaysday" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/alwaysday</h5>
                                     <p className="card-text" style={{ color: "black" }}>是否锁定昼夜更替，等效于/daylock。</p>
@@ -32,7 +32,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/clear" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/clear</h5>
                                     <p className="card-text" style={{ color: "black" }}>清除玩家物品栏中的物品（无法对除玩家以外实体生效）。</p>
@@ -48,7 +48,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/clearspawnpoint" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/clearspawnpoint</h5>
                                     <p className="card-text" style={{ color: "black" }}>清除玩家的重生点。</p>
@@ -64,7 +64,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/clone" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/clone</h5>
                                     <p className="card-text" style={{ color: "black" }}>用于对指定区域内的方块进行复制到另一区域这一操作。</p>
@@ -80,7 +80,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/daylock" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/daylock</h5>
                                     <p className="card-text" style={{ color: "black" }}>是否锁定昼夜更替，等效于/alwaysday。</p>
@@ -96,7 +96,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/deop" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/deop</h5>
                                     <p className="card-text" style={{ color: "black" }}>撤销玩家管理员权限。</p>
@@ -112,7 +112,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/difficulty" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/difficulty</h5>
                                     <p className="card-text" style={{ color: "black" }}>设置游戏的难度。</p>
@@ -128,7 +128,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/enchant" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/enchant</h5>
                                     <p className="card-text" style={{ color: "black" }}>给实体手持物品进行附魔。</p>
@@ -143,7 +143,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/fill" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/fill</h5>
                                     <p className="card-text" style={{ color: "black" }}>该指令可对指定区域进行方块填充。</p>
@@ -159,7 +159,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/gamemode" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/gamemode</h5>
                                     <p className="card-text" style={{ color: "black" }}>用于更改玩家的游戏模式。</p>
@@ -175,7 +175,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/gamerule" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/gamerule</h5>
                                     <p className="card-text" style={{ color: "black" }}>用于更改或查询当前世界的游戏规则。</p>
@@ -191,7 +191,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/give" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/give</h5>
                                     <p className="card-text" style={{ color: "black" }}>给予玩家物品。</p>
@@ -207,7 +207,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/kill" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/kill</h5>
                                     <p className="card-text" style={{ color: "black" }}>对实体进行清除。</p>
@@ -223,7 +223,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/list" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/list</h5>
                                     <p className="card-text" style={{ color: "black" }}>列出服务器当前在线的所有玩家的名称。</p>
@@ -239,7 +239,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/locate" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/locate</h5>
                                     <p className="card-text" style={{ color: "black" }}>向命令发出者显示距离最近的指定结构的坐标。</p>
@@ -255,7 +255,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/msg" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/msg</h5>
                                     <p className="card-text" style={{ color: "black" }}>对其他玩家发送私聊消息。此命令等效于/tell和/w。</p>
@@ -271,7 +271,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/op" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/op</h5>
                                     <p className="card-text" style={{ color: "black" }}>给予玩家管理员权限。</p>
@@ -287,7 +287,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/particle" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/particle</h5>
                                     <p className="card-text" style={{ color: "black" }}>在某位置生成粒子效果。</p>
@@ -303,7 +303,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/playsound" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/playsound</h5>
                                     <p className="card-text" style={{ color: "black" }}>向玩家播放指定声音。</p>
@@ -319,7 +319,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/say" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/say</h5>
                                     <p className="card-text" style={{ color: "black" }}>在聊天栏向所有玩家发送文本消息。</p>
@@ -335,7 +335,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/scoreboard" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/scoreboard</h5>
                                     <p className="card-text" style={{ color: "black" }}>记录某一目标对应的分数信息并提供相应的查询、更改等操作。</p>
@@ -351,7 +351,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/spreadplayers" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/spreadplayers</h5>
                                     <p className="card-text" style={{ color: "black" }}>将实体随机传送至某一指定区域内的随机位置。</p>
@@ -367,7 +367,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/structure" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/structure</h5>
                                     <p className="card-text" style={{ color: "black" }}>其功能类似于结构方块，可对结构进行保存、加载、删除等操作。</p>
@@ -383,7 +383,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/tell" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/tell</h5>
                                     <p className="card-text" style={{ color: "black" }}>对其他玩家发送私聊消息。此命令等效于/msg和/w。</p>
@@ -399,7 +399,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/testfor" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/testfor</h5>
                                     <p className="card-text" style={{ color: "black" }}>检测指定目标的存在与否。</p>
@@ -415,7 +415,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/tickingarea" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/tickingarea</h5>
                                     <p className="card-text" style={{ color: "black" }}>对常加载区块进行操作。</p>
@@ -431,7 +431,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/toggledownfall" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/title</h5>
                                     <p className="card-text" style={{ color: "black" }}>设置当前世界的天气。</p>
@@ -447,7 +447,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/title" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/toggledownfall</h5>
                                     <p className="card-text" style={{ color: "black" }}>title指令可以用于在玩家屏幕上显示文字。</p>
@@ -463,7 +463,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/w" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/w</h5>
                                     <p className="card-text" style={{ color: "black" }}>对其他玩家发送私聊消息。此命令等效于/msg和/tell。</p>
@@ -479,7 +479,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/weather" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/weather</h5>
                                     <p className="card-text" style={{ color: "black" }}>设置当前世界的天气。</p>
@@ -495,7 +495,7 @@ export default function CmdList() {
                 <div className="col-sm-3">
                     <Link href="/commands/xp" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">/xp</h5>
                                     <p className="card-text" style={{ color: "black" }}>给予或扣除玩家的经验。</p>
--- components/child/cmd/other.js
+++ components/child/cmd/other.js
@@ -16,7 +16,7 @@ export default function OtherList() {
                 <div className="col-sm-3">
                     <Link href="/commands/目标选择器" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">目标选择器</h5>
                                     <p className="card-text" style={{ color: "black" }}>它可以指定命令的执行体、条件信息等内容。</p>
@@ -31,7 +31,7 @@ export default function OtherList() {
                 <div className="col-sm-3">
                     <Link href="/commands/权限等级" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">权限等级</h5>
                                     <p className="card-text" style={{ color: "black" }}>权限等级用于控制命令执行者可以执行什么命令。</p>
@@ -46,7 +46,7 @@ export default function OtherList() {
                 <div className="col-sm-3">
                     <Link href="/commands/声音列表" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">声音列表</h5>
                                     <p className="card-text" style={{ color: "black" }}>我的世界可用的声音ID列表。</p>
@@ -61,7 +61,7 @@ export default function OtherList() {
                 <div className="col-sm-3">
                     <Link href="/commands/坐标" passHref>
                         <a>
-                            <div className="card" style={{ height: 160 }}>
+                            <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">坐标</h5>
                                     <p className="card-text" style={{ color: "black" }}>我的世界坐标简介。</p>
